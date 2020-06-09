5afa8de639916904d1ae0d9a9ced47391ca164c0
Vladimir <work.lvg@gmail.com>
2020-06-10 01:53:15 +0400

    little fixes

--- src/Orc.Theming.Example/Views/Wpf/ComboBoxView.xaml.cs
+++ src/Orc.Theming.Example/Views/Wpf/ComboBoxView.xaml.cs
@@ -1,10 +1,17 @@
 ﻿namespace Orc.Theming.Example.Views
 {
+    using System.Windows.Controls;
+
     public partial class ComboBoxView
     {
         public ComboBoxView()
         {
             InitializeComponent();
+        }
+
+        private void Selector_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
         }
     }
 }
