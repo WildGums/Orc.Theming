f68cbe68ce317f98492fcd8171ea1cad846d9e26
Vladimir <work.lvg@gmail.com>
2020-07-06 14:35:40 +0400

    theme tab

--- src/Orc.Theming.Example/ViewModels/Wpf/ListViewViewModel.cs
+++ src/Orc.Theming.Example/ViewModels/Wpf/ListViewViewModel.cs
@@ -15,7 +15,27 @@
                 "Item 2",
                 "Item 3",
                 "Item 4",
-                "Item 5"
+                "Item 5",
+                "Item 6",
+                "Item 7",
+                "Item 8",
+                "Item 9",
+                "Item 10",
+                "Item 11",
+                "Item 12",
+                "Item 13",
+                "Item 14",
+                "Item 15",
+                "Item 16",
+                "Item 17",
+                "Item 18",
+                "Item 19",
+                "Item 20",
+                "Item 21",
+                "Item 22",
+                "Item 23fsdfsdafasdfsadfsdfsadfsdafasfsadfdsafas",
+                "Item 24",
+                "Item 25",
             });
 
             SelectedItem = Items.FirstOrDefault();
