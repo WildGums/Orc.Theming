119bcea5263a0522b611063d6f22019e1227dabf
Vladimir <work.lvg@gmail.com>
2020-07-15 10:34:19 +0400

    treeview style

--- src/Orc.Theming.Example/ViewModels/Wpf/TreeViewViewModel.cs
+++ src/Orc.Theming.Example/ViewModels/Wpf/TreeViewViewModel.cs
@@ -1,15 +1,75 @@
 ﻿namespace Orc.Theming.Example.ViewModels
 {
-    using Catel.MVVM;
+    using System.Collections.Generic;
     using System.Threading.Tasks;
+    using Catel.MVVM;
+
+    public class TreeNode
+    {
+        public string Name { get; set; }
+        public List<TreeNode> ChildNodes { get; set; }
+    }
 
     public class TreeViewViewModel : ViewModelBase
     {
-        public TreeViewViewModel(/* dependency injection here */)
+        public TreeViewViewModel()
         {
+            TreeNodes = new List<TreeNode>
+            {
+                new TreeNode
+                {
+                    Name = "Level_0(1)",
+                    ChildNodes = new List<TreeNode>
+                    {
+                        new TreeNode
+                        {
+                            Name = "Level_1(1)"
+                        },
+
+                        new TreeNode
+                        {
+                            Name = "Level_1(2)"
+                        },
+                    }
+                },
+                new TreeNode
+                {
+                    Name = "Level_0(2)",
+                    ChildNodes = new List<TreeNode>
+                    {
+                        new TreeNode
+                        {
+                            Name = "Level_1(1)"
+                        },
+
+                        new TreeNode
+                        {
+                            Name = "Level_1(2)"
+                        },
+                    }
+                },
+                new TreeNode
+                {
+                    Name = "Level_0(3)",
+                    ChildNodes = new List<TreeNode>
+                    {
+                        new TreeNode
+                        {
+                            Name = "Level_1(1)"
+                        },
+
+                        new TreeNode
+                        {
+                            Name = "Level_1(2)"
+                        },
+                    }
+                }
+            };
         }
 
-        public override string Title { get { return "View model title"; } }
+        public override string Title => "View model title";
+
+        public IList<TreeNode> TreeNodes { get; set; }
 
         // TODO: Register models with the vmpropmodel codesnippet
         // TODO: Register view model properties with the vmprop or vmpropviewmodeltomodel codesnippets
