047702f568b6034080207c34116df4710ea544d6
Vladimir <work.lvg@gmail.com>
2020-09-09 10:48:16 +0400

    little fixes in example

--- src/Orc.Theming.Example/ViewModels/Orc/WizardViewModel.cs
+++ src/Orc.Theming.Example/ViewModels/Orc/WizardViewModel.cs
@@ -3,27 +3,38 @@
     using System.Threading.Tasks;
     using Catel;
     using Catel.MVVM;
-    using Orc.Theming.Example.Wizards.ExampleWizard;
-    using Orc.Wizard;
+    using Catel.Services;
+    using Wizards.ExampleWizard;
+    using Wizard;
 
     public class WizardViewModel : ViewModelBase
     {
         private readonly IWizardService _wizardService;
+        private readonly IUIVisualizerService _uiVisualizerService;
 
-        public WizardViewModel(IWizardService wizardService)
+        public WizardViewModel(IWizardService wizardService, IUIVisualizerService uiVisualizerService)
         {
             Argument.IsNotNull(() => wizardService);
+            Argument.IsNotNull(() => uiVisualizerService);
 
             _wizardService = wizardService;
+            _uiVisualizerService = uiVisualizerService;
 
             ShowWizard = new TaskCommand(OnShowWizardExecuteAsync);
+            ShowSeparateWindow = new TaskCommand(OnShowSeparateWindow);
         }
-
+        
         public TaskCommand ShowWizard { get; private set; }
+        public TaskCommand ShowSeparateWindow { get; private set; }
 
         private async Task OnShowWizardExecuteAsync()
         {
             await _wizardService.ShowWizardAsync<ExampleWizard>();
+        }
+
+        private async Task OnShowSeparateWindow()
+        {
+            await _uiVisualizerService.ShowDialogAsync<SeparateWindowViewModel>();
         }
     }
 }
