e72448f6d73a5580d82b813f3f381c3b17157006
Vladimir <work.lvg@gmail.com>
2020-09-11 03:48:01 +0400

    add usefull color converters

--- src/Orc.Theming/Converters/ProgressBarTrackToIndicatorSizeConverter.cs
+++ src/Orc.Theming/Converters/ProgressBarTrackToIndicatorSizeConverter.cs
@@ -1,10 +1,6 @@
 ﻿namespace Orc.Theming.Converters
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using System.Windows;
     using System.Windows.Media;
     using Catel.MVVM.Converters;
