4b79f9d0ea0907117769d4b5a57e1dde2f69cad6
Vladimir <work.lvg@gmail.com>
2020-09-14 15:23:53 +0400

    ORCOMP-550 fixed First chance exception when closing any window with ThemeSwitcherView

--- src/Orc.Theming/ViewModels/ThemeSwitcherViewModel.cs
+++ src/Orc.Theming/ViewModels/ThemeSwitcherViewModel.cs
@@ -6,7 +6,6 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
-    using System.Windows;
     using System.Windows.Media;
 
     public class ThemeSwitcherViewModel : ViewModelBase
