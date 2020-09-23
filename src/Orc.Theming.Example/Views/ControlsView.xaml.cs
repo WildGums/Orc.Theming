f941ac9989e47c91467964fd31978a55d292249b
Vladimir <work.lvg@gmail.com>
2020-09-23 12:40:47 +0400

    ORCOMP-583 fix link label margin

--- src/Orc.Theming.Example/Views/ControlsView.xaml.cs
+++ src/Orc.Theming.Example/Views/ControlsView.xaml.cs
@@ -1,27 +1,10 @@
 ﻿namespace Orc.Theming.Example.Views
 {
-    using System.Windows;
-    using System.Windows.Input;
-
     public sealed partial class ControlsView
     {
         public ControlsView()
         {
             InitializeComponent();
-        }
-
-        private void btnActiveGradient_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-            var pt = Mouse.GetPosition(btnActiveGradient);
-            gradRadial.SetCurrentValue(System.Windows.Media.RadialGradientBrush.GradientOriginProperty, 
-                new Point(pt.X / btnActiveGradient.Width, pt.Y / btnActiveGradient.Height));
-            gradRadial.SetCurrentValue(System.Windows.Media.RadialGradientBrush.CenterProperty, gradRadial.GradientOrigin);
-        }
-
-        private void btnActiveGradient_MouseLeave(object sender, MouseEventArgs e)
-        {
-            gradRadial.SetCurrentValue(System.Windows.Media.RadialGradientBrush.GradientOriginProperty, new Point(0.5, 0.5));   // Default
-            gradRadial.SetCurrentValue(System.Windows.Media.RadialGradientBrush.CenterProperty, gradRadial.GradientOrigin);
         }
     }
 }
