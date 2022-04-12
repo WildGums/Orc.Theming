37e80061cab3d064cc6e1ea9eb578477407a23c6
Geert van Horrik <geert@catenalogic.com>
2022-04-12 21:58:34 +0200

    Implement font size selector

---
+++ src/Orc.Theming/Services/FontSizeService.cs
@@ -0,0 +1,55 @@
+﻿namespace Orc.Theming
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Controls;
+    using Catel.Logging;
+
+    public class FontSizeService : IFontSizeService
+    {
+        private static readonly ILog Log = LogManager.GetCurrentClassLogger();
+
+        private double? _fontSize;
+
+        public virtual double GetFontSize()
+        {
+            if (!_fontSize.HasValue)
+            {
+                var mainWindow = Application.Current.MainWindow;
+                if (mainWindow is null)
+                {
+                    // default
+                    return 12d;
+                }
+
+                _fontSize = TextBlock.GetFontSize(mainWindow);
+            }
+
+            return _fontSize.Value;
+        }
+
+        public virtual bool SetFontSize(double fontSize)
+        {
+            Log.Info($"Setting font size '{fontSize}'");
+
+            _fontSize = fontSize;
+
+            var mainWindow = Application.Current.MainWindow;
+            if (mainWindow is not null)
+            {
+                TextBlock.SetFontSize(mainWindow, fontSize);
+            }
+
+            RaiseFontSizeChanged();
+
+            return true;
+        }
+
+        public event EventHandler<EventArgs> FontSizeChanged;
+
+        protected void RaiseFontSizeChanged()
+        {
+            FontSizeChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
