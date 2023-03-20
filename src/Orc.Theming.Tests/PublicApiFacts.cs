55025d765010d6c295a79a577854a4be067e1d5d
Vladimir <work.lvg@gmail.com>
2023-03-20 16:37:54 +0300

    fix tests

--- src/Orc.Theming.Tests/PublicApiFacts.cs
+++ src/Orc.Theming.Tests/PublicApiFacts.cs
@@ -1,32 +1,31 @@
-﻿namespace Orc.Theming.Tests
+﻿namespace Orc.Theming.Tests;
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PublicApiGenerator;
+using VerifyNUnit;
+
+[TestFixture]
+public class PublicApiFacts
 {
-    using System.Reflection;
-    using System.Runtime.CompilerServices;
-    using System.Threading.Tasks;
-    using NUnit.Framework;
-    using PublicApiGenerator;
-    using VerifyNUnit;
-
-    [TestFixture]
-    public class PublicApiFacts
-    {
-        [Test, MethodImpl(MethodImplOptions.NoInlining)]
+    [Test, MethodImpl(MethodImplOptions.NoInlining)]
 #pragma warning disable CA1707 // Identifiers should not contain underscores
-        public async Task Orc_Theming_HasNoBreakingChanges_Async()
+    public async Task Orc_Theming_HasNoBreakingChanges_Async()
 #pragma warning restore CA1707 // Identifiers should not contain underscores
-        {
-            var assembly = typeof(ThemeColorBrush).Assembly;
+    {
+        var assembly = typeof(ThemeColorBrush).Assembly;
 
-            await PublicApiApprover.ApprovePublicApiAsync(assembly);
-        }
+        await PublicApiApprover.ApprovePublicApiAsync(assembly);
+    }
 
-        internal static class PublicApiApprover
+    internal static class PublicApiApprover
+    {
+        public static async Task ApprovePublicApiAsync(Assembly assembly)
         {
-            public static async Task ApprovePublicApiAsync(Assembly assembly)
-            {
-                var publicApi = ApiGenerator.GeneratePublicApi(assembly, new ApiGeneratorOptions());
-                await Verifier.Verify(publicApi);
-            }
+            var publicApi = ApiGenerator.GeneratePublicApi(assembly, new ApiGeneratorOptions());
+            await Verifier.Verify(publicApi);
         }
     }
 }
