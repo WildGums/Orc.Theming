4773f710cdef34940af54c7e1b878def099adfaf
Geert van Horrik <geert@catenalogic.com>
2025-04-01 12:45:40 +0200

    Add extra controls for size testing

--- src/SolutionAssemblyInfo.cs
+++ src/SolutionAssemblyInfo.cs
@@ -6,8 +6,8 @@
 using System.Reflection;
 
 [assembly: AssemblyCompany("WildGums")]
-[assembly: AssemblyVersion("5.0.0")]
-[assembly: AssemblyFileVersion("5.0.0")]
-[assembly: AssemblyInformationalVersion("5.0.0-alpha.78")]
+[assembly: AssemblyVersion("5.2.0")]
+[assembly: AssemblyFileVersion("5.2.0")]
+[assembly: AssemblyInformationalVersion("5.2.0-alpha.78")]
 [assembly: AssemblyCopyright("Copyright © WildGums 2020 - 2020")]
 
