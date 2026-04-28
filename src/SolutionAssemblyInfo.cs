2985a94059fb4d6f09467239a785b08a3c3da499
Geert van Horrik <geert@catenalogic.com>
2026-04-28 21:38:51 +0200

    Update

--- src/SolutionAssemblyInfo.cs
+++ src/SolutionAssemblyInfo.cs
@@ -6,8 +6,8 @@
 using System.Reflection;
 
 [assembly: AssemblyCompany("WildGums")]
-[assembly: AssemblyVersion("5.4.0")]
-[assembly: AssemblyFileVersion("5.4.0")]
-[assembly: AssemblyInformationalVersion("5.2.0-alpha.78")]
+[assembly: AssemblyVersion("6.0.0")]
+[assembly: AssemblyFileVersion("6.0.0")]
+[assembly: AssemblyInformationalVersion("6.0.0-alpha.78")]
 [assembly: AssemblyCopyright("Copyright © WildGums 2020 - 2020")]
 
